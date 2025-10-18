3735478a33153db46cc281b96b2ed6cbc3590d3f
alex vartoumain <alex.vartoumian@gmail.com>
2025-10-18 22:50:42 +0100

    checkpoint service layer basic post and patch function

---
+++ routes/users.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const router =  express.Router();
+
+
+const User = require("../models/User")
+
+
+const UserService = require("../service/UserService")
+
+
+router.get("/", async(req , res) => {
+    
+    try{
+        const getFilms = await User.find().limit(10)
+        console.log(getFilms)
+        res.send(getFilms)
+    }catch(err){
+        res.send( {message:err + "oops" })
+    }
+})
+
+// router.post("/" , async(req, res) => {
+//     // console.log("this executed" )
+//     // console.log(req.body.email)
+//     const postData = new User( {
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: req.body.password,
+//         date: Date.now()
+//     })
+//     try{
+//         const postToSave = await postData.save()
+//         res.send(postToSave)
+//     }catch(err){
+//         thing = console.log(req.body.name)
+//         res.send({message:err, thing})
+//     }
+// })
+
+router.post("/" , async(req, res) => {
+   
+    try{
+        payload = { name : req.body.name, email : req.body.email, password: req.body.password}
+
+        const userToSave = await UserService.createUser(payload)
+        
+         res.status(201).json({ 
+            success: true, 
+            userToSave 
+        });
+    }catch(err){
+        thing = console.log(req.body.name)
+        res.send({message:err, thing})
+    }
+})
+
+
+
+router.get("/:id", async(req , res) => {
+    
+    //PostService = new PostService()
+    const id = req.params.id;
+    console.log(id)
+    try{
+        const getFilms = await User.findById(req.params.id)
+        
+        res.send(getFilms)
+    }catch(err){
+        res.send( {message:err + "oops" })
+    }
+})
+
+
+
+module.exports = router
+
+
---
+++ service/PostService.js
@@ -0,0 +1,85 @@
+const Post = require("../models/Post");
+
+//https://www.reddit.com/r/rails/comments/itivdn/where_did_concept_of_service_object_come_from/
+
+//https://stackoverflow.com/questions/63218568/what-should-be-structure-of-service-layer-in-node-js
+
+
+class PostService {
+
+    async createPost(userId, { title, topic, body, expirationMinutes = 60 }) {
+
+        try{
+            const post = new Post({
+                title,
+                topic,
+                body,
+                owner: userId,
+                expiration: new Date(Date.now() + expirationMinutes * 60 * 1000)
+            });
+            
+            await post.save();
+            return post; 
+        } catch (error){
+            return error
+        }
+     }
+
+     async addComment(postId, userId , body) {
+        
+        try{
+            const post = await Post.findById(postId)
+            // payload = {  userId : userId, timestamp: Date.now(), comment: body }
+            // post.comments.push(payload)
+        
+            post.comments.push({  user : userId, timestamp: Date.now(), comment: body })
+            await post.save()
+            return post
+        } catch (error){
+            return error
+        }
+     }
+    
+     async likePost(postId, userId) {
+
+        const post = await Post.findById(postId)
+
+        //https://www.w3schools.com/jsref/jsref_filter.asp use predicate to filter existing id 
+        post.dislikes = post.dislikes.filter(id => !id.equals(userId));
+        
+        if (!post.likes.some(id => id.equals(userId))) {
+            post.likes.push(userId);
+        }
+
+        await post.save()
+        
+        return post
+
+    }
+    async dislikePost(postId, userId) {
+
+        const post = await Post.findById(postId)
+
+        //https://www.w3schools.com/jsref/jsref_filter.asp use predicate to filter existing id 
+        post.likes = post.likes.filter(id => !id.equals(userId));
+        console.log(post.likes)
+        console.log(post.dislikes)
+        if (!post.dislikes.some(id => id.equals(userId))) {
+            post.dislikes.push(userId);
+            
+        }
+        await post.save()
+        
+        return post
+     }
+
+     
+   
+}
+
+
+module.exports = new PostService()
+
+
+
+
